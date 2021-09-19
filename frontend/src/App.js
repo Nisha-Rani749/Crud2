4558762231c9a18795f37ebef68638a42259b9b2
Nisha-gif227 <csnisha21@gmail.com>
2021-09-19 22:13:52 +0530

    mernapp

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import Inputstudent from './components/Inputstudent'
+import Liststudent from './components/Liststudent'
+function App() {
+  return (
+    <div>
+      <Inputstudent/>
+      <Liststudent/>
+    </div>
+  );
+}
+
+export default App;
